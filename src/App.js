d6ae50703c820d05a6f642f3afd6151340fea172
poconri <pocon.ri@gmail.com>
2021-10-15 00:04:15 -0600

    adding the HTML in React

--- src/App.js
+++ src/App.js
@@ -1,8 +1,31 @@
+import React from "react";
+import { TodoCounter } from './TodoCounter';
+import { TodoSearch } from "./TodoSearch";
+import { TodoList } from "./TodoList";
+import { CreateTodoButton } from "./CreateTodoButton";
+import { TodoItem } from "./TodoItem";
 // import './App.css';
+
+const todos = [
+  {text:'Cortar cebolla', completed:false},
+  {text:'Tocar el curso de intro de React', completed:false},
+  {text:'Llorar con la llorona', completed:false},
+];
 
 function App() {
   return (
-    
+    <React.Fragment>
+    <TodoCounter />
+    <TodoSearch />
+
+    <TodoList>
+      {todos.map(todo => (
+        <TodoItem key={todo.text} text={todo.text} />
+      ))}
+    </TodoList>
+
+    <CreateTodoButton />
+  </React.Fragment>
   );
 }
 
---
+++ src/TodoCounter.js
@@ -0,0 +1,10 @@
+import React from "react";
+import './todoCounter.css';
+
+function TodoCounter () {
+    return (
+        <h2 className="todoCounter">Has Completado 2 de 3 TODOs</h2>
+    );
+}
+
+export { TodoCounter };
