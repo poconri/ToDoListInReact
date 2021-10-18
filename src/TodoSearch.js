7e29f3a1f693a0c9a1975d9d4a5654f0534ab8fa
poconri <pocon.ri@gmail.com>
2021-10-18 00:47:03 -0600

    adding react hooks

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,17 +1,25 @@
 import React from "react";
-import './TodoSearch.css'
+import './TodoSearch.css';
+
+
 
 function TodoSearch () {
-    const estado = React.useState();
+    const [searchValue, SetSearchValue] = React.useState('');
+
+
 
     const onSearchValueChange = (event) => {
         console.log(event.target.value);
+        SetSearchValue(event.target.value);
     };
-    return <input
+    return [<input
     placeholder="Cebolla"
     className='TodoSearch'
+    value={searchValue}
     onChange={onSearchValueChange}
-     />
+     />,
+     <p>{searchValue}</p>
+    ];
 }
 
 export { TodoSearch };
